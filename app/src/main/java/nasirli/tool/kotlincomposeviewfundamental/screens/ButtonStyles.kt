7863e4112651e3ef825b27d72bf34219fdaf9154
ulvinasirli99 <ulvinasirli99@gmail.com>
2024-08-05 08:07:13 +0400

    some fundamentals added

---
+++ app/src/main/java/nasirli/tool/kotlincomposeviewfundamental/screens/ButtonStyles.kt
@@ -0,0 +1,10 @@
+package nasirli.tool.kotlincomposeviewfundamental.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview(showBackground = true)
+fun ButtonStyles() {
+
+}
--- app/src/main/java/nasirli/tool/kotlincomposeviewfundamental/screens/RowColumnWidget.kt
+++ app/src/main/java/nasirli/tool/kotlincomposeviewfundamental/screens/RowColumnWidget.kt
@@ -1,2 +1,35 @@
 package nasirli.tool.kotlincomposeviewfundamental.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+@Preview(showBackground = true)
+fun RowAndColumnWidget() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        Text(text = "Text One", fontSize = 18.sp)
+        Text(text = "Text Two", fontSize = 18.sp)
+        Text(text = "Text Third", fontSize = 18.sp)
+    }
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(text = "Text One", fontSize = 18.sp)
+        Text(text = "Text Two", fontSize = 18.sp)
+        Text(text = "Text Third", fontSize = 18.sp)
+    }
+}
