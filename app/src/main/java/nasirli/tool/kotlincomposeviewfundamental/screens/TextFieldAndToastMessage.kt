a95b4670788efb2b9264e4e0b5d649f89fc681bf
Ulvi R. Nasirov <URNasirov@bankofbaku.com>
2024-08-12 14:39:24 +0400

    text_field and toast message added

--- app/src/main/java/nasirli/tool/kotlincomposeviewfundamental/screens/TextFieldAndToastMessage.kt
+++ app/src/main/java/nasirli/tool/kotlincomposeviewfundamental/screens/TextFieldAndToastMessage.kt
@@ -1,9 +1,22 @@
 package nasirli.tool.kotlincomposeviewfundamental.screens
 
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -13,28 +26,119 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 @Preview(showBackground = true)
 fun TextFieldAndToastMessage() {
+
+    var newSurnameValue by remember {
+        mutableStateOf("")
+    }
+
+    var newPasswordValue by remember {
+        mutableStateOf("")
+    }
+
+    val context = LocalContext.current
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    val focusManager = LocalFocusManager.current
+
+
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable(
+                indication = null, // Disables the click effect
+                interactionSource = remember { MutableInteractionSource() }
+            ) {
+                focusManager.clearFocus()
+            },
         contentAlignment = Alignment.Center,
     ) {
-        var newTextValue by remember {
-            mutableStateOf("")
-        }
+
         Column(verticalArrangement = Arrangement.spacedBy(25.dp)) {
             TextField(
-                value = newTextValue,
+                value = newSurnameValue,
                 onValueChange = {
-                    newTextValue = it
+                    newSurnameValue = it
                 },
                 label = {
                     Text(text = "Enter your Surname")
-                }
+                },
+                singleLine = true,
+                placeholder = {
+                    Text(text = "Ulvi Nasirov")
+                },
+                leadingIcon = {
+                    Icon(imageVector = Icons.Outlined.AccountCircle, contentDescription = "")
+                },
+                trailingIcon = {
+                    IconButton(onClick = {
+                        Toast.makeText(context, "Enter your Surname", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(imageVector = Icons.Outlined.Add, contentDescription = "")
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.Characters,
+                    keyboardType = KeyboardType.Number,
+                    imeAction = ImeAction.Done,
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        keyboardController?.hide()
+                        focusManager.clearFocus()
+                    }
+                )
+            )
+            OutlinedTextField(
+                value = newPasswordValue,
+                onValueChange = {
+                    newPasswordValue = it
+                },
+                label = {
+                    Text(text = "Enter your Password")
+                },
+                singleLine = true,
+                placeholder = {
+                    Text(text = "Test1234")
+                },
+                visualTransformation = PasswordVisualTransformation(),
+                leadingIcon = {
+                    IconButton(onClick = {/*TODO*/ }) {
+                        Icon(imageVector = Icons.Outlined.Settings, contentDescription = "")
+                    }
+                },
+                trailingIcon = {
+                    IconButton(onClick = {
+                        Toast.makeText(context, "Enter your password", Toast.LENGTH_SHORT).show()
+                    }) {
+                        Icon(imageVector = Icons.Outlined.Check, contentDescription = "")
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    capitalization = KeyboardCapitalization.Characters,
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Go,
+                ),
+                keyboardActions = KeyboardActions(
+                    onGo = {
+                        keyboardController?.hide()
+                        focusManager.clearFocus()
+                    }
+                )
             )
         }
     }
